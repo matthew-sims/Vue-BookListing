0c02291755dd72da8fddbdc964edc21eebbc909e
Matthew Sims <msims@intertech.com>
2019-06-26 13:43:24 -0500

    BookList 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
+    </ul>
   </div>
 </template>
 
@@ -8,6 +11,12 @@
   export default {
     name: 'BookList',
     data: () => {
+      const books = [
+        {title: 'The Chronicles of Narnia', author: 'C.S. Lewis'},
+        {title: 'Harry Potter', author: 'J.K. Rowling'},
+        {title: 'Inheritance Cycle', author: 'Christopher Paolini'},
+      ];
+
       return { title: 'All Books' };
     }
   }
@@ -16,5 +25,15 @@
 <style scoped>
   h1, h2 {
     font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  ul li {
+    display: block;
+    margin: 0 10px;
   }
 </style>
